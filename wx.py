13caa0fceda0fd5a81ba3e15831c9754010eb7ac
祀画 <827169070@qq.com>
2021-08-19 11:46:33 +0800

    Update wx.py

--- wx.py
+++ wx.py
@@ -1,7 +1,7 @@
 import requests
 
 res = requests.get('https://dldir1.qq.com/weixin/android/weixin_android_alpha_config.json')
-
+print(res.text)
 f = open("demofile3.txt", "w")
 f.write(res.text)
 f.close()
